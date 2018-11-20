3b2410317aa2f67064c72e99c560275aa177e6c2
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-20 16:59:53 -0500

    Finishes tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,42 @@
+let cat, dog, bird, bird2;
+
 describe("cat", () => {
+  beforeEach(() => {
+    cat = new Cat("Sasha", "female")
+  })
+
   it("has a class of cat", () => {
-    expect(Cat).to.exist
+    expect(new Cat()).to.be.an.instanceof(Cat);
+  })
+  it("cat.speak() returns '[name]' says meow!", () => {
+    expect(cat.speak()).to.eq(`${cat.name} says meow!`)
   })
 })
 describe("dog", () => {
+  beforeEach(() => {
+    dog = new Dog("Rufio", "male")
+  })
+
   it("has a class of dog", () => {
-    expect(Dog).to.exist
+    expect(new Dog()).to.be.an.instanceof(Dog);
+  })
+  it("dog.speak() returns '[name] says woof!'", () => {
+    expect(dog.speak()).to.eq(`${dog.name} says woof!`)
   })
 })
 describe("bird", () => {
+  beforeEach(() => {
+    bird = new Bird("Pablo", "male")
+    bird2 = new Bird("Mable", "female")
+  })
+  
   it("has a class of Bird", () => {
-    expect(Bird).to.exist
+    expect(new Bird()).to.be.an.instanceof(Bird);
+  })
+  it("bird.speak() returns 'It's me! [name]], the parrot!'", () => {
+    expect(bird.speak()).to.eq(`It's me! ${bird.name}, the parrot!`)
+  })
+  it("bird2.speak() returns 'Squawk!'", () => {
+    expect(bird2.speak()).to.eq(`Squawk!`)
   })
 })
