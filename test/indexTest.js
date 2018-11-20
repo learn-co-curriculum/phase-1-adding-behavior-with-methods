2664d044945b54f95d8c9c4b47ca7a940f318995
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-20 16:41:38 -0500

    Adds starting tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,15 @@
-
-
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
+describe("cat", () => {
+  it("has a class of cat", () => {
+    expect(Cat).to.exist
   })
-
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
-
-
 })
-
-
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
+describe("dog", () => {
+  it("has a class of dog", () => {
+    expect(Dog).to.exist
+  })
+})
+describe("bird", () => {
+  it("has a class of Bird", () => {
+    expect(Bird).to.exist
   })
 })
